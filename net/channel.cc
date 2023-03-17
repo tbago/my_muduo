03cddc5ad3d71ded1e1b924d2f3ca68887596aa2
tbago <moonzalor@gmail.com>
2023-03-17 23:04:16 +0800

    Add channel implement

--- net/channel.cc
+++ net/channel.cc
@@ -1,5 +1,82 @@
 #include "channel.h"
 
+#include <sys/epoll.h>
+
+#include "base/logger.h"
+#include "base/time_stamp.h"
+#include "event_loop.h"
+
 namespace net {
+
+const int Channel::kNoneEvent = 0;
+const int Channel::kReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::kWriteEvent = EPOLLOUT;
+
+Channel::Channel(EventLoop *event_loop, int fd) : event_loop_(event_loop), fd_(fd) {
+    events_ = 0;
+    return_events_ = 0;
+    index_ = -1;
+    tied_ = -1;
+}
+
+Channel::~Channel() {
+}
+
+void Channel::HandleEvent(base::TimeStamp receive_time) {
+    if (tied_) {
+        std::shared_ptr<void> guard = tie_.lock();
+        if (guard) {
+            HandleEventWithGuard(receive_time);
+        }
+        // 如果提升失败了 就不做任何处理 说明Channel的TcpConnection对象已经不存在了
+    } else {
+        HandleEventWithGuard(receive_time);
+    }
+}
+
+// channel的tie方法什么时候调用过?  TcpConnection => channel
+/**
+ * TcpConnection中注册了Channel对应的回调函数，传入的回调函数均为TcpConnection
+ * 对象的成员方法，因此可以说明一点就是：Channel的结束一定早于TcpConnection对象！
+ * 此处用tie去解决TcoConnection和Channel的生命周期时长问题，从而保证了Channel对
+ * 象能够在TcpConnection销毁前销毁。
+ **/
+void Channel::Tie(const std::shared_ptr<void> &object) {
+    tie_ = object;
+    tied_ = true;
+}
+
+void Channel::Remove() {
+    event_loop_->ReomveChannel(this);
+}
+
+void Channel::Update() {
+    event_loop_->UpdateChannel(this);
+}
+
+void Channel::HandleEventWithGuard(base::TimeStamp receive_time) {
+    LOG_INFO("Channel handle event return events :%d", return_events_);
+
+    if ((return_events_ & EPOLLHUP) && !(return_events_ & EPOLLIN)) {
+        if (close_event_callback_) {
+            close_event_callback_();
+        }
+    }
+    if (return_events_ & EPOLLERR) {
+        if (error_event_callback_) {
+            error_event_callback_();
+        }
+    }
+    if (return_events_ & (EPOLLIN | EPOLLPRI)) {
+        if (read_event_callback_) {
+            read_event_callback_();
+        }
+    }
+    if (return_events_ & EPOLLOUT) {
+        if (write_event_callback_) {
+            write_event_callback_();
+        }
+    }
+}
 
 }  // namespace net
--- net/channel.h
+++ net/channel.h
@@ -1,9 +1,77 @@
 #pragma once
 
+#include <functional>
+#include <memory>
+
+#include "base/none_copyable.h"
+#include "base/time_stamp.h"
+
 namespace net {
 
+class EventLoop;
+
+/**
+ * Channel 封装了sockfd和其相关的event ，例如EPOLLIN, EPOLLOUT。还绑定了poller返回的具体事件。
+ **/
 class Channel {
 public:
+    Channel(EventLoop *event_loop, int fd);
+    ~Channel();
+
+public:
+    using ReadEventCallback = std::function<void(base::TimeStamp)>;  ///< 读时间回调
+    using EventCallback = std::function<void()>;                     ///< 其他事件回调
+
+    void SetReadEventCallback(ReadEventCallback callback) { read_event_callback_ = std::move(callback); }
+    void SetWriteEventCallback(EventCallback callback) { write_event_callback_ = std::move(callback); }
+    void SetCloseEventCallback(EventCallback callback) { close_event_callback_ = std::move(callback); }
+    void SetErrorEventCallback(EventCallback callback) { error_event_callback_ = std::move(callback); }
+
+    // fd得到Poller通知以后 处理事件 handleEvent在EventLoop::loop()中调用
+    void HandleEvent(base::TimeStamp receive_time);
+public:
+    // 设置fd相应的时间状态，相当于epoll_ctl add delete
+    void EnableReading() { events_ |= kReadEvent; Update(); }
+    void DisableReading() { events_ &= ~kReadEvent; Update(); }
+    void EnableWritting() { events_ |= kWriteEvent; Update(); }
+    void DisableWritting() { events_ &= ~kWriteEvent; Update(); }
+    void DisableAll() { events_ = kNoneEvent; Update(); }
+    // 获取当前fd的时间状态
+    bool IsNoneEvent() const { return events_ == kNoneEvent; }
+    bool CanWritting() const { return events_ & kWriteEvent; }
+    bool CanReading() const { return events_ & kReadEvent; }
+public:
+    int Fd() const { return fd_; }
+    int Events() const { return events_; }
+    void SetReturnEvents(int return_events) { return_events_ = return_events; }
+    int Index() const { return index_; }
+    void SetIndex(int index) {index_ = index; }
+    EventLoop *GetOwnerLoop() const { return event_loop_; }
+    void Remove();
+public:
+    void Tie(const std::shared_ptr<void> &);  // 防止当channel被手动remove掉时，channel还在执行回调操作
+private:
+    void Update();
+    void HandleEventWithGuard(base::TimeStamp receive_time);
+private:
+    static const int kNoneEvent;
+    static const int kReadEvent;
+    static const int kWriteEvent;
+
+private:
+    ReadEventCallback read_event_callback_;
+    EventCallback write_event_callback_;
+    EventCallback close_event_callback_;
+    EventCallback error_event_callback_;
+
+    EventLoop *event_loop_;  // 时间循环
+    const int fd_;           // fd Poller 监听的对象
+    int events_;
+    int return_events_;
+    int index_;
+
+    std::weak_ptr<void> tie_;
+    bool tied_;
 };
 
 }  // namespace net
